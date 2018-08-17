e462d42debb3b9c15492dbad3ba7bd38854a5bad
David Betgen <db@yes-co.nl>
2018-08-17 14:44:03 +0200

    Added: DOM Document Handler

---
+++ src/Dom/Node.php
@@ -0,0 +1,15 @@
+<?php
+
+  /**
+   * Node
+   *
+   * Extention of DOMNode
+   *
+   * @author David Betgen <d.betgen@remote-office.nl>
+   * @version 1.0
+   */
+  class Node extends DOMNode
+  {
+  }
+
+?>
