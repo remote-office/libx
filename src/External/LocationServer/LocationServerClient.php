3d59e62511229cecae1929415f8f862dcdcfc498
David Betgen <db@yes-co.nl>
2022-01-12 16:19:13 +0100

    Quickfix for district and neighbourhood

--- src/External/LocationServer/LocationServerClient.php
+++ src/External/LocationServer/LocationServerClient.php
@@ -1,9 +1,9 @@
 <?php
 
   namespace LibX\External\LocationServer;
-
+  
   use Exception;
-
+  
   /**
    * Location Server Client
    *
@@ -14,7 +14,7 @@
   {
     // Location server API url
     const API_URL = 'https://geodata.nationaalgeoregister.nl/locatieserver/v3';
-
+    
     /**
      * Suggest an address id
      *
@@ -27,31 +27,31 @@
     {
       // Concat url
       $url = self::API_URL . '/suggest?q=' . $zipcode . '+' . $number . (!empty($addition) ? '+' . $addition : '');
-
+      
       // Create a REST request
       $request = new \LibX\Net\Rest\Request($url, \LibX\Net\Rest\Request::REQUEST_METHOD_GET);
-
+      
       // Create a REST response
       $response = new \LibX\Net\Rest\Response();
-
+      
       // Make the call
       $data = $this->call($request, $response);
-
+      
       if($data->response->numFound == 0)
         throw new Exception('No document found');
-
+        
       if($data->response->numFound > 1)
       {
         $document = null;
         $score = 0;
-
+        
         foreach($data->response->docs as $doc)
         {
           if($doc->score > $score && $doc->type === 'adres')
           {
             // Update score
             $score = $doc->score;
-
+            
             $document = $doc;
           }
         }
@@ -61,21 +61,20 @@
         // Get document
         $document = array_pop($data->response->docs);
       }
-
+      
       //if($data->response->numFound > 1)
-        //throw new Exception('More then one document found');
-
+      //throw new Exception('More then one document found');
+      
       // Check type
       if($document->type !== 'adres')
         throw new Exception('Invalid document type found (' . $document->type . ')');
-
+        
       // Get address id
       $id = $document->id;
-
+      
       return $id;
-
     }
-
+    
     /**
      * Lookup an address id
      *
@@ -86,37 +85,46 @@
     {
       // Concat url
       $url = self::API_URL . '/lookup?id=' . $id;
-
+      
       // Create a REST request
       $request = new \LibX\Net\Rest\Request($url, \LibX\Net\Rest\Request::REQUEST_METHOD_GET);
-
+      
       // Create a REST response
       $response = new \LibX\Net\Rest\Response();
-
+      
       // Make the call
       $data = $this->call($request, $response);
-
+      
       if($data->response->numFound == 0)
         throw new Exception('No document found');
-
+        
       if($data->response->numFound > 1)
         throw new Exception('More then one document found');
-
+        
       // Get document
       $document = array_pop($data->response->docs);
-
+      
       // Check type
       if($document->type !== 'adres')
         throw new Exception('Invalid document type found (' . $doc->type . ')');
-
+            
       $state = $document->provincienaam;
       $municipality = $document->gemeentenaam;
       $city = $document->woonplaatsnaam;
-      $area = $document->wijknaam;
-      $neighbourhood = $document->buurtnaam;
+      
+      if(isset($document->wijknaam))
+        $area = $document->wijknaam;
+      else
+        $area = 'onbekend';
+                
+      if(isset($document->buurtnaam))
+        $neighbourhood = $document->buurtnaam;
+      else  
+        $neighbourhood = 'onbekend';
+                    
       $street = $document->straatnaam;
       $number = $document->huisnummer;
-
+                    
       if(isset($document->huisnummertoevoeging) && isset($document->huisletter))
         $addition = $document->huisnummertoevoeging . $document->huisletter;
       elseif(isset($document->huisnummertoevoeging))
@@ -125,9 +133,9 @@
         $addition = $document->huisletter;
       else
         $addition = null;
-
+      
       $zipcode = $document->postcode;
-
+      
       if($document->bron === 'BAG')
       {
         $address = new \LibX\External\Bag\Address();
@@ -135,8 +143,17 @@
         $address->setStateCode($document->provinciecode);
         $address->setMunicipalityCode($document->gemeentecode);
         $address->setCityCode($document->woonplaatscode);
-        $address->setDistrictCode($document->wijkcode);
-        $address->setNeighbourhoodCode($document->buurtcode);
+        
+        if(isset($document->wijkcode))
+          $address->setDistrictCode($document->wijkcode);
+        else
+          $address->setDistrictCode('000000');
+            
+        if(isset($document->buurtcode))
+          $address->setNeighbourhoodCode($document->buurtcode);
+        else
+          $address->setNeighbourhoodCode('00000000');
+          
         $address->setPublicSpaceId($document->openbareruimte_id);
         $address->setNumberIndicationId($document->nummeraanduiding_id);
         $address->setAddressableObjectId($document->adresseerbaarobject_id);
@@ -145,7 +162,7 @@
       {
         $address = new \LibX\Util\Address();
       }
-
+      
       $address->setCountry('Nederland');
       $address->setState($state);
       $address->setMunicipality($municipality);
@@ -156,9 +173,9 @@
       $address->setNumber($number);
       $address->setAddition($addition);
       $address->setZipcode($zipcode);
-
+      
       return $address;
     }
   }
-
+  
 ?>
